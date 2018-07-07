6acea49fa96d8e2c22d836b1ae0c05099ffd61c7
mattpilla <kaztalek@gmail.com>
2018-07-07 16:58:16 -0400

    convert ui build script to bash to prevent weirdness

---
+++ scripts/ui.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Script to build UI
+# Run from root directory of project
+
+# Start timer
+start=`date +%s`;
+
+# Abort on error
+set -e;
+
+# Make temp directory
+rm -rf tmp;
+mkdir tmp;
+
+# Clone UI and build it
+cd tmp;
+git clone https://github.com/mattpilla/kaztalek-ui.git .;
+npm install;
+npm run build;
+
+# Replace old UI build with new one
+cd ..;
+cp -Trv tmp/dist ui;
+rm -rf tmp;
+
+end=`date +%s`;
+echo finished in $((end-start)) seconds;
