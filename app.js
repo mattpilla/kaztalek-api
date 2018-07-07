90196ec266c571694077b91f0a5226e5bad94f17
mattpilla <kaztalek@gmail.com>
2018-07-07 14:58:06 -0400

    serve index

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const serveIndex = require('serve-index');
 const app = express();
 const port =  process.env.PORT || 51581;
 
+app.use('/files', serveIndex('public/files', {
+    icons: true,
+    stylesheet: './resources/style.css',
+    template: './resources/directory.html'
+}));
 app.use(express.static('public'));
 app.use(express.static('ui'));
 
@@ -10,7 +16,7 @@ app.get('/1pause', (req, res) => {
     res.send('No.');
 });
 
-require('./redirects')(app);
+require('./routes/redirects')(app);
 
 app.listen(port, () => {
     console.log(`listening on port ${port}\nlets do this shit`);
